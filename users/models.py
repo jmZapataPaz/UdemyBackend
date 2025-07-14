bd4ce19a243622fd7bec6784ec608804da7148f4
lordvader991 <josemariazapata12@gmail.com>
2025-07-14 09:12:08 -0400

    first commit

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lastname= models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, unique=True)
+    image =models.CharField(max_length=255, null=True, blank=False) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
